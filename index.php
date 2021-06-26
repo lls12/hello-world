40227772ce02b76331e5242258e8175607428c52
Umedjon Aliev <umed1717s@gmail.com>
2021-06-26 20:40:02 +0500

    update index.php with emergency fix

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+   h1>Hello world!</h1>
+<p>This is the first file in my new Git Repo.</p>
+<p>This line is here to show how merging works.</p>
+ 
+</body>
+</html>
+
 <?php
 
 $a = 1;
@@ -11,3 +29,4 @@ function test(&$a){
 $a1 = 8;
 echo test($a1);
 echo "<br>";
+?>
