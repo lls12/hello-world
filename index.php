b0c1b675e4fd20a795d9ac1365bd62a02e8c7966
Umedjon Aliev <umed1717s@gmail.com>
2021-06-26 20:24:23 +0500

    added image to hello world

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+    <img src="https://source.unsplash.com/random" alt="">
+
 <?php
 
 $a = 1;
@@ -11,3 +26,4 @@ function test(&$a){
 $a1 = 8;
 echo test($a1);
 echo "<br>";
+?>
